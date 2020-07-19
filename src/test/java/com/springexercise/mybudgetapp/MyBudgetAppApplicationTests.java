2e8b17810b1ff8c161cfbde462cfccb871f3efc6
geodan89 <geodan89@gmail.com>
2020-07-19 15:48:16 +0300

    created the initial project.

---
+++ src/test/java/com/springexercise/mybudgetapp/MyBudgetAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springexercise.mybudgetapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBudgetAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
