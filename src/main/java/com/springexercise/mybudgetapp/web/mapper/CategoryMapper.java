bd6740f3d3cf1ce2039b5e9d52e5d8eb31b0aec6
geodan89 <geodan89@gmail.com>
2020-08-15 22:39:12 +0300

    Added CategoryMapper.INSTANCE

--- src/main/java/com/springexercise/mybudgetapp/web/mapper/CategoryMapper.java
+++ src/main/java/com/springexercise/mybudgetapp/web/mapper/CategoryMapper.java
@@ -3,9 +3,12 @@ package com.springexercise.mybudgetapp.web.mapper;
 import com.springexercise.mybudgetapp.domain.Category;
 import com.springexercise.mybudgetapp.web.model.CategoryDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface CategoryMapper {
+
+    CategoryMapper INSTANCE = Mappers.getMapper(CategoryMapper.class);
 
     CategoryDto categoryToCategoryDto(Category category);
 
