eb34e6e2351a31851cc3783b44918c5743c798ab
geodan89 <geodan89@gmail.com>
2020-08-20 00:06:31 +0300

    Implemented all Expense endpoints. closes #14, #18, #19, #20, #21.

--- src/main/java/com/springexercise/mybudgetapp/web/controller/ExpenseController.java
+++ src/main/java/com/springexercise/mybudgetapp/web/controller/ExpenseController.java
@@ -21,13 +21,11 @@ public class ExpenseController {
 
     private final ExpenseService expenseService;
 
-    /**
+    /*
      * Get all expenses from a category
-     **/
+     */
     @GetMapping("/category/{categoryId}/expenses")
     public ResponseEntity<ExpenseListDto> getAllExpenses(@PathVariable("categoryId") String categoryId) {
-        //To be properly implemented.
-
         Long catId = Long.valueOf(categoryId);
         List<ExpenseDto> list = expenseService.getAllExpenses(catId);
         ExpenseListDto expenseListDto = new ExpenseListDto();
@@ -36,42 +34,36 @@ public class ExpenseController {
         return new ResponseEntity<>(expenseListDto, HttpStatus.OK);
     }
 
-    /**
+    /*
      * Get a certain expense from a category
-     **/
+     */
     @GetMapping("/category/{categoryId}/expense/{expenseId}")
     public ResponseEntity<ExpenseDto> getExpenseById(@PathVariable("categoryId") String categoryId, @PathVariable("expenseId") String expenseId) {
-        //to be implemented;
-
         log.debug("I'm in controller GET method...");
         Long expId = Long.valueOf(expenseId);
         Long catId = Long.valueOf(categoryId);
         return new ResponseEntity<>(expenseService.findByCategoryIdAndExpenseId(catId, expId), HttpStatus.OK);
     }
 
-    /**
+    /*
      * Create a new expense in a category
-     **/
+     */
     @PostMapping("/category/{categoryId}/expense")
     public ResponseEntity<ExpenseDto> createNewExpense(@PathVariable String categoryId, @RequestBody ExpenseDto expenseDto) {
-        //To be properly implemented.
-
         Long catId = Long.valueOf(categoryId);
         ExpenseDto savedExpenseDto = expenseService.saveNewExpense(catId, expenseDto);
 
         HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add("Location", "/api/v1/expense/" + savedExpenseDto.getId().toString());
+        httpHeaders.add("Location", "/api/v1/category/" + categoryId + "/expense/" + savedExpenseDto.getId().toString());
         return new ResponseEntity<>(httpHeaders, HttpStatus.CREATED);
     }
 
-    /**
+    /*
      * Update an expense in a category
-     **/
+     */
     @PutMapping({"/category/{categoryId}/expense/{expenseId}"})
     public ResponseEntity<ExpenseDto> updateExpense(@PathVariable("categoryId") String categoryId,
                                                     @PathVariable("expenseId") String expenseId, @RequestBody @Valid ExpenseDto expenseDto) {
-        //To be properly implemented.
-
         Long expId = Long.valueOf(expenseId);
         Long catId = Long.valueOf(categoryId);
         return new ResponseEntity<>(expenseService.updateExpense(catId, expId, expenseDto), HttpStatus.OK);
@@ -83,13 +75,12 @@ public class ExpenseController {
 //        return new ResponseEntity<>(expenseService.updateExpense(expenseId, expenseDto), HttpStatus.OK);
 //    }
 
-    /**
+    /*
      * Delete an expense from a category
-     **/
+     */
     @DeleteMapping({"/category/{categoryId}/expense/{expenseId}"})
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteExpenseById(@PathVariable("categoryId") String categoryId, @PathVariable("expenseId") String expenseId) {
-        //To be properly implemented.
         Long expId = Long.valueOf(expenseId);
         Long catId = Long.valueOf(categoryId);
         expenseService.deleteById(catId, expId);
